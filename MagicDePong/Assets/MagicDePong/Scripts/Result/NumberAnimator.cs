8c096838277429156065ab70eb6edc14e9c94734
yoshimoto <seiya.yoshimoto@gree.net>
2015-10-31 15:23:45 +0900

    number animation

---
+++ MagicDePong/Assets/MagicDePong/Scripts/Result/NumberAnimator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberAnimator : MonoBehaviour {
+	private Text text;
+	private int frame;
+	private int scoreNow;
+	private int scoreTo;
+
+	void Start ()
+	{
+		text = GetComponent<Text>();
+		frame = 0;
+		scoreTo = 100;
+		scoreNow = 0;
+	}
+
+	void Update ()
+	{
+		if (scoreNow >= scoreTo)
+		{
+			return;
+		}
+		frame++;
+		if (frame % 1 == 0)
+		{
+			scoreNow++;
+			UpdateNumber();
+		}
+	}
+	private void UpdateNumber()
+	{
+		text.text = scoreNow.ToString();
+	}
+}
