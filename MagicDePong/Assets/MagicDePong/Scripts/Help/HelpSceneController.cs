33e2b1dcf8f74ef4c70b90e3590ce0d559b49292
murapong <murapong@gmail.com>
2015-10-31 12:57:33 +0900

    雛型の準備

---
+++ MagicDePong/Assets/MagicDePong/Scripts/Help/HelpSceneController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpSceneController : MonoBehaviour {
+	#region enum
+
+	#endregion
+
+	#region const
+
+	#endregion
+
+	#region public property
+
+	#endregion
+
+	#region private property
+
+	#endregion
+
+	#region public method
+
+	#endregion
+
+	#region private method
+
+	#endregion
+
+	#region event
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+
+	#endregion
+}
