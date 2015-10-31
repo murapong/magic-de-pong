de670e51562e7b0992455d3966a1c83d89b42ad9
murapong <murapong@gmail.com>
2015-10-31 13:28:50 +0900

    SoundManagerを追加

---
+++ MagicDePong/Assets/MagicDePong/Scripts/Common/SoundManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+	#region enum
+
+	#endregion
+
+	#region const
+
+	#endregion
+
+	#region public property
+
+	public static SoundManager Instance;
+
+	#endregion
+
+	#region private property
+
+	#region AudioClip
+
+	/// <summary>
+	/// 決定音。
+	/// </summary>
+	[SerializeField]
+	AudioClip audioClipYesSE;
+
+	/// <summary>
+	/// ゲームオーバーSE。
+	/// </summary>
+	[SerializeField]
+	AudioClip audioClipGameOverSE;
+
+	#endregion
+
+	#region AudioSource
+
+	AudioSource audioSourceYes;
+
+	AudioSource audioSourceGameOver;
+
+	#endregion
+
+	#endregion
+
+	#region public method
+
+	/// <summary>
+	/// 決定音を再生する。
+	/// </summary>
+	public void PlayYesSE ()
+	{
+		audioSourceYes.Play ();
+	}
+
+	/// <summary>
+	/// ゲームオーバーSEを再生する。
+	/// </summary>
+	public void PlayGameOverSE ()
+	{
+		audioSourceGameOver.Play ();
+	}
+
+	#endregion
+
+	#region private method
+
+	#endregion
+
+	#region event
+
+	void Awake ()
+	{
+		Instance = this;
+
+		InitSound ();
+	}
+
+	/// <summary>
+	/// サウンドの初期化を行う。
+	/// </summary>
+	void InitSound ()
+	{
+		audioSourceYes = gameObject.AddComponent<AudioSource> ();
+		audioSourceYes.clip = audioClipYesSE;
+		audioSourceYes.loop = false;
+
+		audioSourceGameOver = gameObject.AddComponent<AudioSource> ();
+		audioSourceGameOver.clip = audioClipGameOverSE;
+		audioSourceGameOver.loop = false;
+	}
+
+	#endregion
+}
