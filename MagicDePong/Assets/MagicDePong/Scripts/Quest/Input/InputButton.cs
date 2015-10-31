745101e02f6b8a15424f4a1869b2b8436c606022
yoshimoto <seiya.yoshimoto@gree.net>
2015-10-31 18:02:16 +0900

    create input

---
+++ MagicDePong/Assets/MagicDePong/Scripts/Quest/Input/InputButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InputButton : Button, ICanvasRaycastFilter, IPointerEnterHandler {
+    public const int SIZE_WIDTH = 82;
+    public const int SIZE_HEIGHT = 82;
+    public const int SIZE_GAP = 15;//画像自体のサイズと円の部分との隙間
+    private RectTransform rectTransform;
+    private Image image;
+
+    public void Start()
+    {
+        rectTransform = GetComponent (typeof (RectTransform)) as RectTransform;
+        rectTransform.sizeDelta = new Vector2(SIZE_WIDTH, SIZE_WIDTH);
+        image = GetComponent<Image>();
+    }
+    public bool IsRaycastLocationValid (Vector2 sp, Camera eventCamera)
+    {
+        return Vector2.Distance(sp, transform.position) < rectTransform.sizeDelta.x;
+    }
+    public void OnPointerEnter(PointerEventData ped)
+    {
+        image.color = Color.black;
+    }
+}
---
+++ MagicDePong/Assets/MagicDePong/Scripts/Quest/Input/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+    private const int NUM_WIDTH = 5;
+    private const int NUM_HEIGHT = 5;
+    private Vector3 basePosition;
+    GameObject prefab;
+	void Start () {
+	   prefab = (GameObject)Resources.Load ("Prefabs/Input/InputButton");
+       basePosition =  new Vector3(32, 30, 0);
+       for (int i = 0; i < NUM_WIDTH; i++)
+       {
+            for (int j = 0; j < NUM_HEIGHT; j++)
+            {
+                Vector3 pos = GetButtonPosition(i, j);
+                GameObject obj = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
+                obj.transform.SetParent(transform);
+                obj.name = "button_" + i.ToString() + "_" + j.ToString();
+            }
+       }
+	}
+
+	void Update ()
+    {
+	}
+    private Vector3 GetButtonPosition(int i, int j)
+    {
+        return basePosition + new Vector3(i * (InputButton.SIZE_WIDTH - InputButton.SIZE_GAP),
+            j * (InputButton.SIZE_HEIGHT - InputButton.SIZE_GAP), 0);
+    }
+}
