a6d75600fa04bde1da52a42cc8729cdf890d6daf
nosenosenose <nose.straighter@gmail.com>
2015-11-01 16:47:14 +0900

    config

--- MagicDePong/Assets/MagicDePong/Data/ConfigData.cs
+++ MagicDePong/Assets/MagicDePong/Data/ConfigData.cs
@@ -6,13 +6,13 @@ public class ConfigData
 	//魔法陣の判定用ボタンを表示するかどうか
 	public static bool ShowMagicButton = true;
 	//戦闘時間
-	public static int BattleTime = 50;
+	public static int BattleTime = 120;
     //属性が有利な時のダメージ
     public static float StrongElementDamage = 2f;
     //属性が不利な時のダメージ
     public static float WeakElementDamage = 0.2f;
     public static int Rare1Damage = 50;
-    public static int Rare2Damage = 100;
-    public static int Rare3Damage = 300;
+    public static int Rare2Damage = 150;
+    public static int Rare3Damage = 400;
     public static int Rare4Damage = 1000;
 }
