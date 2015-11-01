5b84f99391c70560d82474b6f57ca0e1d33c86b5
murapong <murapong@gmail.com>
2015-11-01 17:40:16 +0900

    スキル画像の追加

--- MagicDePong/Assets/MagicDePong/Scripts/Quest/SkillIcon.cs
+++ MagicDePong/Assets/MagicDePong/Scripts/Quest/SkillIcon.cs
@@ -2,37 +2,43 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class SkillIcon : MonoBehaviour {
+public class SkillIcon : MonoBehaviour
+{
     private Image image;
     private Vector3 startPosition;
     private float time;
-	void Start () {
+
+    void Start()
+    {
         image = GetComponent<Image>();
         image.enabled = false;
         startPosition = transform.position;
-	}
+    }
 
-    public void Show(int skillId, int width, int height)
+    public void Show(int skillId, int widt, int height)
     {
-        GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        GetComponent<RectTransform>().sizeDelta = new Vector2(720, 164);
         image.enabled = true;
         transform.position = startPosition;
         image.sprite = Resources.Load("Quest/skills/" + skillId.ToString(), typeof(Sprite)) as Sprite;
         time = 0.8f;
-    }	
-	void Update () {
+    }
+
+    void Update()
+    {
         if (!image.enabled || time <= 0)
             return;
         time -= Time.deltaTime;
         if (time <= 0)
         {
             Hashtable table = new Hashtable();  
-            table.Add("x", - 500.1f);
+            table.Add("x", -500.1f);
             table.Add("time", 0.5f);
             table.Add("oncomplete", "CompleteHandler");
             iTween.MoveBy(gameObject, table);
         }
-	}
+    }
+
     private void CompleteHandler()
     {
         image.enabled = false;
