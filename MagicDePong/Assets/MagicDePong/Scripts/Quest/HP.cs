ef374e3781864a10bb66a665b4e06719fb2fe77e
yoshimoto <seiya.yoshimoto@gree.net>
2015-11-01 11:13:20 +0900

    show hp

---
+++ MagicDePong/Assets/MagicDePong/Scripts/Quest/HP.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour {
+    [SerializeField]
+    private Slider slider;
+    private float point = 1.0f;
+    public void SetPercent(float percent)
+    {
+        slider.value = percent;
+    }
+	
+	void Update () {
+	   point*= 0.95f;
+       SetPercent(point);
+	}
+}
