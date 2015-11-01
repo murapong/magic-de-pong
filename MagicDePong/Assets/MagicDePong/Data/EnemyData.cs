685de52ff140a3b9bd0919845a1714972e54a676
yoshimoto <seiya.yoshimoto@gree.net>
2015-11-01 14:40:01 +0900

    use enemyData

---
+++ MagicDePong/Assets/MagicDePong/Data/EnemyData.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyData
+{
+    public int hp;
+    public SkillData.Element element;
+
+    public static EnemyData Get(int id)
+    {
+        if (id == 2)
+        return null;
+        switch(id)
+        {
+            case 0:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 1:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 2:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 3:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 4:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 5:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 6:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 7:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 8:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 9:  return new EnemyData(  10, SkillData.Element.Fire);
+            case 10: return new EnemyData(  10, SkillData.Element.Fire);
+            case 11: return new EnemyData(  10, SkillData.Element.Fire);
+            case 12: return new EnemyData(  10, SkillData.Element.Fire);
+            case 13: return new EnemyData(  10, SkillData.Element.Fire);
+            case 14: return new EnemyData(  10, SkillData.Element.Fire);
+            case 15: return new EnemyData(  10, SkillData.Element.Fire);
+        }
+        return null;
+    }
+
+    private EnemyData(int hp, SkillData.Element element)
+    {
+        this.hp = hp;
+        this.element = element;
+    }
+}
